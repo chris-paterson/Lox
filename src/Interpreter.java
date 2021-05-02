8639e0be1e7a4d5c17d4f09f21dd02bc8afc238c
Chris Paterson <cjpaterson.3@gmail.com>
2021-05-02 11:58:23 -0400

    Concat as strings if left or right is string for addition

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -23,6 +23,11 @@ public class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof String && right instanceof String) {
                     return (String)left + (String)right;
                 }
+
+                if (left instanceof String || right instanceof String) {
+                    return stringify(left) + stringify(right);
+                }
+
                 throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case MINUS:
                 checkNumberOperands(expr.operator, left, right);
